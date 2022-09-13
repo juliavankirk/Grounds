4a9b2a7b28be81a0b7596de932e79236d928a4cb
Sergey B <sergeyb@student.chalmers.se>
2022-09-13 11:35:19 +0200

    Create user, delete user by id, Create product, list of products, Delete product by id, Create order, Get list of orders routes implemented. Postman test for Create user.

--- server/app.js
+++ server/app.js
@@ -4,13 +4,20 @@ var morgan = require('morgan');
 var path = require('path');
 var cors = require('cors');
 var history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser');
+
+var app = express();
+var Schema = mongoose.Schema;
 
 // Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/animalDevelopmentDB';
+var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/groundsDB';
+//'mongodb+srv://sergeyb:Pfgflyfz69@cluster0.i1nkq8s.mongodb.net/mydb?retryWrites=true&w=majority';
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
 // Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
     if (err) {
         console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
         console.error(err.stack);
@@ -18,6 +25,44 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, 
     }
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
 });
+
+// Mongoose schema
+var userSchema = new Schema({
+    forename: { type: String },
+    surename: { type: String },
+    id: { type: Number },
+    role: { type: String },
+    email: { type: String },
+    password: { type: String }
+});
+
+var productSchema = new Schema({
+    product_id: { type: Number },
+    name: { type: String },
+    quantity: { type: Number },
+    description: { type: String }
+});
+
+var cartSchema = new Schema({
+    cart_id: { type: Number },
+    total: { type: Number },
+    quantity: { type: Number }
+});
+
+var orderSchema = new Schema({
+    order_id: { type: Number },
+    country: { type: String },
+    city: { type: String },
+    street: { type: String },
+    postcode: { type: Number },
+    contact: { type: String }
+});
+
+//Mongoose model
+var User = mongoose.model('users', userSchema);
+var Product = mongoose.model('products', productSchema);
+var Cart = mongoose.model('carts', cartSchema);
+var Order = mongoose.model('orders', orderSchema);
 
 // Create Express app
 var app = express();
@@ -31,9 +76,83 @@ app.options('*', cors());
 app.use(cors());
 
 // Import routes
-app.get('/api', function(req, res) {
-    res.json({'message': 'Welcome to your DIT342 backend ExpressJS project!'});
+app.get('/api', function (req, res) {
+    res.json({ 'message': 'Welcome to your DIT342 backend ExpressJS project!' });
 });
+
+// POST - Create a new user
+app.post('/users', function (req, res, next) {
+    var user = new User(req.body);
+    user.save(function (err) {
+        if (err) { return next(err); }
+        res.status(201).json(user);
+    });
+});
+
+// DELETE - Delete the user by given id
+app.delete('/users/:id', function (req, res, next) {
+    var id = req.params.id;
+    User.findOneAndDelete({ _id: id }, function (err, user) {
+        if (err) { return next(err); }
+        if (user == null) {
+            return res.status(404).json(
+                { "message": "User not found" });
+        }
+        res.json(user);
+    });
+});
+
+//POST - Create a new product
+app.post('/products', function (req, res, next) {
+    var product = new Product(req.body);
+    product.save(function (err) {
+        if (err) { return next(err); }
+        res.status(201).json(product);
+    });
+});
+
+//GET - Read the list of products
+app.get('/products', function (req, res, next) {
+    Product.find(function (err, products) {
+        if (err) { return next(err); }
+        res.status(201).json(products);
+    });
+});
+
+// DELETE - Delete the product by given id
+app.delete('/products/:id', function (req, res, next) {
+    var id = req.params.id;
+    Product.findOneAndDelete({ _id: id }, function (err, product) {
+        if (err) { return next(err); }
+        if (product == null) {
+            return res.status(404).json(
+                { "message": "Product not found" });
+        }
+        res.json(product);
+    });
+});
+
+//POST - Create a new order
+app.post('/orders', function (req, res, next) {
+    var order = new Order(req.body);
+    order.save(function (err) {
+        if (err) { return next(err); }
+        res.status(201).json(order);
+    });
+});
+
+//GET - Read the list of orders
+app.get('/orders', function (req, res, next) {
+    Order.find(function (err, orders) {
+        if (err) { return next(err); }
+        res.status(201).json(orders);
+    });
+});
+
+//DELETE - Delete the list of orders
+app.delete('/orders', function(req, res, next){
+    
+})
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function (req, res) {
@@ -51,7 +170,7 @@ app.use(express.static(client));
 // Error handler (i.e., when exception is thrown) must be registered last
 var env = app.get('env');
 // eslint-disable-next-line no-unused-vars
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     var err_res = {
         'message': err.message,
@@ -65,7 +184,7 @@ app.use(function(err, req, res, next) {
     res.json(err_res);
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
     if (err) throw err;
     console.log(`Express server listening on port ${port}, in ${env} mode`);
     console.log(`Backend: http://localhost:${port}/api/`);
