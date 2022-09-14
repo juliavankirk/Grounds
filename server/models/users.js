5388663be8eaaef4a1b9a25095f4a37846370a68
juliavankirk <juliavankirk@gmail.com>
2022-09-14 15:13:00 +0200

    sets up models for db collections

---
+++ server/models/users.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    forename: {
+        type: String,
+        required: true
+     },
+    surname: {
+        type: String,
+        required: true
+     },
+    role: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
