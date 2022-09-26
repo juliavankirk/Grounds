d612ced625b8f5c4145b4760ece62c328e9b1669
juliavankirk <juliavankirk@gmail.com>
2022-09-26 03:35:42 +0200

    fixed home page to be more responsive

---
+++ client/src/components/HomePage/Category.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="category">
+    <img
+      v-if="product === 'coffee'"
+      src="../../assets/img/cat-thumbnail-coffee.png"
+      alt="coffee"
+      class="category__thumbnail"
+      id="coffee"
+    />
+    <img
+      v-if="product === 'brewer'"
+      src="../../assets/img/cat-thumbnail-brewer.png"
+      alt="brewers"
+      class="category__thumbnail"
+      id="brewers"
+    />
+    <img
+      v-if="product === 'mugs'"
+      src="../../assets/img/cat-thumbnail-mug.png"
+      alt="mugs"
+      class="category__thumbnail"
+      id="mugs"
+    />
+    <h2 class="category__name">{{ product }}</h2>
+    <router-link
+      class="category__link"
+      :to="{ name: 'CategoryPage', params: { category: `${product}` } }"
+      @click="scrollToTop"
+    >
+      Shop
+      <img
+        src="../../assets/svg/icon-arrow-right.svg"
+        class="category__link__icon"
+      />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: {
+    product: String,
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f1f1f1;
+  margin: 0 auto 6.8rem auto;
+  border-radius: 0.8rem;
+  max-width: 32.7rem;
+
+  @media (min-width: 768px) {
+    margin: 0;
+    margin-right: 1rem;
+    max-width: auto;
+    min-width: 22.3rem;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+
+  @media (min-width: 1205px) {
+    margin-right: 3rem;
+    min-width: 35rem;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  & #brewers {
+    top: -4.6rem;
+  }
+
+  & #mugs {
+    top: -4rem;
+  }
+
+  &__thumbnail {
+    width: 15rem;
+    position: relative;
+    top: -5.2rem;
+    margin-bottom: -5.2rem;
+
+    @media (min-width: 1205px) {
+      width: 17rem;
+    }
+  }
+
+  &__name {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 2.049rem;
+    letter-spacing: 0.107rem;
+
+    @media (min-width: 1205px) {
+      font-size: 1.8rem;
+      line-height: 2.459rem;
+      letter-spacing: 0.129rem;
+    }
+  }
+
+  &__link {
+    margin-top: 1.7rem;
+    margin-bottom: 2.2rem;
+    color: #797979;
+    font-size: 1.3rem;
+    font-weight: 700;
+    line-height: 1.776rem;
+    letter-spacing: 0.1rem;
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: #d87d4a;
+    }
+
+    &__icon {
+      margin-left: 1.332rem;
+    }
+  }
+}
+</style>
--- client/src/components/Menu.vue
+++ client/src/components/Menu.vue
@@ -1,129 +1,83 @@
 <template>
-  <div class="category">
-    <img
-      v-if="product === 'coffee'"
-      src="../assets/img/cat-thumbnail-coffee.png"
-      alt="coffee"
-      class="category__thumbnail"
-      id="coffee"
-    />
-    <img
-      v-if="product === 'brewer'"
-      src="../assets/img/cat-thumbnail-brewer.png"
-      alt="brewer"
-      class="category__thumbnail"
-      id="brewer"
-    />
-    <img
-      v-if="product === 'mug'"
-      src="../assets/img/cat-thumbnail-mug.png"
-      alt="mug"
-      class="category__thumbnail"
-      id="mug"
-    />
-    <h2 class="category__name">{{ product }}</h2>
-    <router-link
-      class="category__link"
-      @click="$emit('toggle-menu-show', 'menu')"
-      :to="{ name: 'CategoryPage', params: { category: `${product}` } }"
-    >
-      Shop
-      <img
-        src="../assets/svg/icon-arrow-right.svg"
-        class="category__link__icon"
-      />
-    </router-link>
+<div>
+  <div :class="['overlay', show ? 'showElement' : 'hideElement']"></div>
+  <div :class="['menu', show ? 'showElement' : 'hideElement']" ref="mobileMenu">
+    <section class="navbar">
+      <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+        <b-nav-item href="/catalog">Shop</b-nav-item>
+        <b-nav-item href="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </section>
   </div>
+</div>
 </template>
 
 <script>
+
 export default {
-  name: "Category",
-  props: {
-    product: String,
-    show: Boolean,
-  },
+  name: "Menu",
+  props: { show: Boolean, scrollTop: Boolean },
   emits: ["toggle-menu-show"],
+  watch: {
+    scrollTop() {
+      this.$refs.mobileMenu.scrollTo(0, 0);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.category {
+.overlay {
+  background: black;
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  z-index: 1;
+  position: absolute;
+  opacity: 0.4;
+
+  @media (min-width: 1205px) {
+    display: none !important;
+  }
+}
+
+.menu {
+  position: absolute;
+  background: white;
+  width: 100%;
+  top: 9.1rem;
+  padding: 8.4rem 0 3.5rem 0;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background: #f1f1f1;
-  margin: 0 auto 6.8rem auto;
-  border-radius: 0.8rem;
-  width: 32.7rem;
+  z-index: 2;
+  opacity: 1;
+  max-height: 75vh;
+  overflow-y: auto;
 
   @media (min-width: 768px) {
-    margin: 0;
-    margin-right: 1rem;
-    width: 22.3rem;
-
-    &:last-child {
-      margin-right: 0;
-    }
+    max-height: auto;
+    padding: 10.8rem 0 6.7rem 0;
   }
 
-  &:last-child {
-    margin-bottom: 0;
+  @media (min-width: 1205px) {
+    display: none !important;
   }
+}
 
-  & #brewer {
-    top: -4.6rem;
-  }
-
-  & #mug {
-    top: -4rem;
-  }
-
-  &__thumbnail {
-    width: 15rem;
-    position: relative;
-    top: -5.2rem;
-    margin-bottom: -5.2rem;
-
-    @media (min-width: 1205px) {
-      width: 17rem;
-    }
-  }
-
-  &__name {
-    text-transform: uppercase;
-    font-weight: 700;
-    font-size: 1.5rem;
-    line-height: 2.049rem;
-    letter-spacing: 0.107rem;
-
-    @media (min-width: 1205px) {
-      font-size: 1.8rem;
-      line-height: 2.459rem;
-      letter-spacing: 0.129rem;
-    }
-  }
-
-  &__link {
-    margin-top: 1.7rem;
-    margin-bottom: 2.2rem;
-    color: #797979;
-    font-size: 1.3rem;
-    font-weight: 700;
-    line-height: 1.776rem;
-    letter-spacing: 0.1rem;
-    text-transform: uppercase;
+.navbar {
+  @media (min-width: 768px) {
     display: flex;
     align-items: center;
-    transition: all 0.3s ease;
-
-    &:hover {
-      color: #d87d4a;
-    }
-
-    &__icon {
-      margin-left: 1.332rem;
-    }
   }
+}
+
+.showElement {
+  display: flex;
+}
+
+.hideElement {
+  display: none;
 }
 </style>
