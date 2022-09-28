9713078201ae9a58e8c2d1b34a613563d0874f4e
Sergey B <sergeyb@student.chalmers.se>
2022-09-28 12:41:07 +0200

    changed login using username to email, minor changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,13 +5,11 @@ dotenv.config();
 const CryptoJS = require("crypto-js"); // Encryption library
 const jwt = require("jsonwebtoken"); // Library that generates token for each user, for more security
 
-// //REGISTER
+//REGISTER
 router.post("/register", async (req, res) => {
   const candidate = await User.findOne({ email: req.body.email });
   if (candidate) {
-    res.status(409).json({
-      message: 'User with this email has been already registered'
-    })
+    res.status(409).json({ message: 'The email has been already registered' })
   } else {
     // Save newUser to db as a promise
     const newUser = new User({
@@ -31,7 +29,7 @@ router.post("/register", async (req, res) => {
       // Save to client side / 201 means successfully edited
       res.status(201).json(newUser);
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json({ message: 'Internal server error' });
     }
   }
 })
@@ -39,44 +37,68 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post('/login', async (req, res) => {
   try {
-    const user = await User.findOne( // Find user withing db
-      {
-        userName: req.body.user_name
+    const user = await User.findOne({ email: req.body.email }); // Find user withing db
+
+    if (user) {
+      const hashedPassword = CryptoJS.AES.decrypt( // Decrypt password
+        user.password,
+        process.env.PASS_SEC);
+
+      const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8); // Parameter used for special characters
+      const inputPassword = req.body.password;
+
+      if (originalPassword == inputPassword) {
+        const accessToken = jwt.sign( // Once information checks
+          {
+            id: user._id,
+            isAdmin: user.isAdmin,
+          },
+          process.env.JWT_SEC,
+          //Access token expires every three days    
+          { expiresIn: "3d" }
+        );
+        const { password, ...others } = user._doc; // Pass in mongoDB document
+        return res.status(200).json({ ...others, accessToken }); // Hides password from being shown
+      } else {
+        res.status(401).json({ message: 'Wrong password' })
       }
-    );
-
-    if (!user) return res.status(401).json("Incorrrect username!");
-
-    const hashedPassword = CryptoJS.AES.decrypt( // Decrypt password
-      user.password,
-      process.env.PASS_SEC
-    );
-
-
-    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8); // Parameter used for special characters
-    const inputPassword = req.body.password;
-
-    if (originalPassword != inputPassword) {
-      return res.status(401).json("Incorrect password!");
+    } else {
+      res.status(404).json({ message: 'User not found' })
     }
-
-    const accessToken = jwt.sign( // Once information checks
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_SEC,
-      // Access token expires every three days
-      { expiresIn: "3d" }
-    );
-
-    const { password, ...others } = user._doc; // Pass in mongoDB document
-    return res.status(200).json({ ...others, accessToken }); // Hides password from being shown
-
   } catch (err) {
-    return res.status(500).json(err);
+    return res.status(500).json({ message: 'Internal server error' });
   }
+})
 
-});
+//   if (!user) return res.status(401).json("User not found!");
+
+//   const hashedPassword = CryptoJS.AES.decrypt( // Decrypt password
+//     user.password,
+//     process.env.PASS_SEC
+//   );
+
+//   const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8); // Parameter used for special characters
+//   const inputPassword = req.body.password;
+
+//   if (originalPassword != inputPassword) {
+//     return res.status(401).json("Incorrect password!");
+//   }
+
+//   const accessToken = jwt.sign( // Once information checks
+//     {
+//       id: user._id,
+//       isAdmin: user.isAdmin,
+//     },
+//     process.env.JWT_SEC,
+//     // Access token expires every three days
+//     { expiresIn: "3d" }
+//   );
+
+//   const { password, ...others } = user._doc; // Pass in mongoDB document
+//   return res.status(200).json({ ...others, accessToken }); // Hides password from being shown
+
+// } catch (err) {
+//   return res.status(500).json({ message: 'Internal server error' });
+// }
 
 module.exports = router;
